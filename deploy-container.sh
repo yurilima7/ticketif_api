46ec18149ffc1c73ca409643e00371d9e8fa1d4c
Thiago Nascimento Pereira <thiago.pereira@ifma.edu.br>
2024-07-19 09:19:58 -0300

    script bash para atualizar o sistema no servidor

--- deploy-container.sh
+++ deploy-container.sh
@@ -1,4 +1,14 @@
+APP_NAME="ticketif-api"
+# O volume é onde ficará o banco de dados
+VOLUME_MAPPING=ticketif-data:/sqlite_db
+
+VERSAO_ANTERIOR=$(docker image ls | grep $APP_NAME | tr -s ' ' | cut -d ' ' -f 2 | sort -rn | head -n 1)
+NOVA_VERSAO=$((VERSAO_ANTERIOR+1))
+
+IMAGE_NAME=$APP_NAME:$NOVA_VERSAO
+echo $IMAGE_NAME
+docker build -t $IMAGE_NAME .
 
 
-
-docker run --name ticketif-api -p 8088:8000 -d -v ticketif-data:/sqlite_db ticketif-api:latest
+docker container rm --force $APP_NAME
+docker run --name $APP_NAME -p 8088:8000 -d -v $VOLUME_MAPPING $IMAGE_NAME
